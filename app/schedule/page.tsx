e054781ca5df0d01de70d42ec3765ad543e4c129
Anesthesia <anastasiamarinopoulos@gmail.com>
2023-10-01 17:52:54 -0700

    Moved getLocalData() to central location.
    Somehow, I had duplicatet this code into multiple files.  Now, it is as it should be; in just one file making it easy to reuse and maintain.

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link"
-
-import fsPromises from 'fs/promises';
-import path from 'path'
+import {getLocalData} from '../../_components/ReadJson';
 
 export default async function Schedule() {
     const schedule = await getLocalData('json/schedule.json');
@@ -37,19 +35,3 @@ export default async function Schedule() {
     )
 }
 
-
-export async function getLocalData(fileName: string) {
-    // Get the path of the json file
-    const filePath = path.join(process.cwd(), fileName);
-    // console.log(filePath);
-    
-    // Read the json file
-    const jsonDataBytes = await fsPromises.readFile(filePath);
-  
-    // DEBUG return jsonDataStr ;
-  
-    // Convert the bytes into a string and then parse that string into a JS object
-    const objectData = JSON.parse(jsonDataBytes.toString());
-  
-    return objectData;
-  }
--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link"
-
-import fsPromises from 'fs/promises';
-import path from 'path'
+import {getLocalData} from '../../_components/ReadJson';
 
 export default async function Teams() {
     const teams = await getLocalData('json/teams.json');
@@ -50,19 +48,3 @@ export default async function Teams() {
     )
 }
 
-
-export async function getLocalData(fileName: string) {
-    // Get the path of the json file
-    const filePath = path.join(process.cwd(), fileName);
-    // console.log(filePath);
-    
-    // Read the json file
-    const jsonDataBytes = await fsPromises.readFile(filePath);
-  
-    // DEBUG return jsonDataStr ;
-  
-    // Convert the bytes into a string and then parse that string into a JS object
-    const objectData = JSON.parse(jsonDataBytes.toString());
-  
-    return objectData;
-  }
